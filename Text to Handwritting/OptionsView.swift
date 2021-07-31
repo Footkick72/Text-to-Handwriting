c81f64750ec169126dce4418ea8a327784f819ea
Daniel Long <zhozholong@gmail.com>
2021-07-31 12:18:14 -0400

    deleted extraneous, commented out code in OptionsView

--- Text to Handwritting/OptionsView.swift
+++ Text to Handwritting/OptionsView.swift
@@ -8,61 +8,6 @@
 import Foundation
 import SwiftUI
 
-//struct OptionsView: View {
-//    @Binding var document: Text_to_HandwritingDocument
-//    @Binding var shown: Bool
-//
-//    var body: some View {
-//        VStack {
-//            testView()
-//            testView()
-//        }
-//    }
-//}
-//
-//struct testView: View {
-//    @State var showingAlert1 = false
-//    @State var showingAlert2 = false
-//    @State var showingSelector = false
-//
-//    var body: some View {
-//        HStack {
-//            Button(action: {
-//                var name = "Untitled"
-//                var i = 0
-//                while FileManager.default.fileExists(atPath: FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(name + ".tthcharset").path) {
-//                    i += 1
-//                    name = "Untitled " + String(i)
-//                }
-//                let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(name + ".tthcharset")
-//                showingAlert1 = true
-//            }) {
-//                Image(systemName: "doc.badge.plus")
-//            }
-//            .alert(isPresented: $showingAlert1) {
-//                Alert(title: Text("File created"), message: Text("The file has been created and added to your filesystem"), dismissButton: .default(Text("OK")))
-//            }
-//
-//            Button(action: {
-//                showingAlert2 = true
-//            }) {
-//                Image(systemName: "square.and.arrow.down")
-//            }
-//            .alert(isPresented: $showingAlert2) {
-//                Alert(title: Text("Cannot load file"), message: Text("You have already loaded an identical file"), dismissButton: .default(Text("OK")))
-//            }
-//        }
-//        ScrollView(.horizontal) {
-//            HStack(alignment: .center) {
-//                ForEach(0..<100) {
-//                    Text("Row \($0)")
-//                }
-//            }
-//        }
-//        .fileImporter(isPresented: $showingSelector, allowedContentTypes: [.charSetDocument]) { url in }
-//    }
-//}
-
 struct OptionsView: View {
     @ObservedObject var charsets = CharSets
     @ObservedObject var templates = Templates
