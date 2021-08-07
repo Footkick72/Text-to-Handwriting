dacc989957011db51ad02382f87ef648e67ce9e9
Daniel Long <zhozholong@gmail.com>
2021-08-06 20:19:39 -0400

    Edited instructions; made instructions get regenerated on every load to allow for updates.

--- Text to Handwritting/AutoDocumentCreator.swift
+++ Text to Handwritting/AutoDocumentCreator.swift
@@ -16,24 +16,25 @@ let DocumentCreator = AutoDocumentCreator()
 class AutoDocumentCreator {
     
     let files: Dictionary<String, Data> = ["EmptyCharacterSet.tthcharset": try! JSONEncoder().encode(CharSet(characters: Dictionary<String, Array<PKDrawing>>(), charlens: Dictionary<String, Float>(), letterSpacing: 4)),
-                                           "EmptyTemplate.tthtemplate": try! JSONEncoder().encode(Template(bg: UIImage(imageLiteralResourceName: "blankpaper.png"), margins: CGRect(x: 50, y: 50, width: 750, height: 1000), size: 30, textColor: [0.0, 0.0, 0.0, 1.0], writingStyle: "Pen")),
-                                           "Instructions.txt": """
+                                           "EmptyTemplate.tthtemplate": try! JSONEncoder().encode(Template(bg: UIImage(imageLiteralResourceName: "blankpaper.png"), margins: CGRect(x: 50, y: 50, width: 750, height: 1000), size: 30, textColor: [0.0, 0.0, 0.0, 1.0], writingStyle: "Pen"))]
+    
+    let instructions = """
                                             Welcome to Text to Handwriting!
-
+                                            
                                             This application converts typed text into images of handwritten text. To see it in action, tap the "Convert to handwriting" button at the top right of this document, and then tap "Save to Photos". A handwritten version of this file will be added to your photos.
-
-                                            The generator supports simple markup. To make a word appear *bold*, place a "*" on either end of it. You can also _underline_ with two "_"s or use two "~"s for ~strikethrough~.
-
+                                            
+                                            The generator supports simple markup. You can place a pair of "*"s on either end of **any text you want to bold**. You can also __underline text__ with two "_"s or use two "~"s for ~~strikethrough~~. You can also combine these modifiers together to create **~~__an effect like this.__~~**.
+                                            
                                             The generation can be customized using the menu that appears after tapping "Convert to Handwriting". There is a character set that gives the handwriting style, and a template that gives the paper style. Tap on them to select a different character set or template. There are two default templates, for lined and unlined paper, and one default character set. The import button is used to select a new character set or template file from your filesystem.
-
+                                            
                                             An empty character set and a blank template are automatically created in your filesystem. If these are deleted or renamed, they will be recreated upon app launch. These files are provided for creating your own character sets or template. They can be imported into the generator and used as described above.
-
+                                            
                                             The template editor allows you to select a background image from your photos. Then select where the text should be drawn on that image by dragging the center or corners of the red rectangle that appears. At the bottom there are options for text size, text color, and the type of writing tool.
-
+                                            
                                             The character set editor allows you to enter your own handwriting samples. The colors in the main view represent the completeness of the character set. Characters highlighted in red have no samples and will appear as blank, yellow characters have between 1 and 4 samples, and green characters have at least 5 samples. The more samples provided, the more variety in the generated text. Tap a character to open the writing view.
-
+                                            
                                             In the writing view, there is a canvas for writing in the center. The character to write is shown at the top. The buttons above the canvas save the drawing, go the previous character, go to the next character, or clear the canvas. Every time an image is saved, it will be added to the scrolling view of images. To delete an image, tap the the "x" button on the top right of its preview.
-                                            """.data(using: .utf8)!]
+                                            """.data(using: .utf8)!
     
     func createDocuments() {
         for (path, file) in files {
@@ -42,6 +43,9 @@ class AutoDocumentCreator {
                 try! file.write(to: url)
             }
         }
+        
+        let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("Instructions.txt")
+        try! instructions.write(to: url)
         
         for (url, file) in CharSetDocument.defaults {
             let data = try! JSONEncoder().encode(file)
